ac3a91999c100a1507f113f0f743f66a28ff48bb
Andrew Candela <andrew.candela@lambdaschool.com>
2020-12-24 10:49:31 -0800

    fix api gateway deployment

--- terraform/lambda_functions.tf
+++ terraform/lambda_functions.tf
@@ -81,3 +81,25 @@ resource "aws_lambda_function" "crypto_email" {
     }
   }
 }
+
+
+# Schedule the lambda function
+resource "aws_cloudwatch_event_rule" "every_one_minute" {
+  name                = "every-one-minute"
+  description         = "Fires every one minutes"
+  schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "run_batch_job" {
+  rule      = aws_cloudwatch_event_rule.every_one_minute.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.crypto_batch.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_crypto_batch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.crypto_batch.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.every_one_minute.arn
+}
